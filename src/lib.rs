f30e7bccc6a70b554ddf47c30409544677cd8cb6
trujimat <mateo.trujillo@fing.edu.uy>
2024-10-06 23:50:21 -0300

    logic for just one cell

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,35 @@
 pub mod cell {
     pub struct Cell {
-        state: State,
-        position: (i32, i32),
+        pub current_state: State,
+        pub previous_state: State,
+        pub position: (i32, i32),
     }
 
-    enum State {
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum State {
         Alive,
         Dead,
     }
 
     impl Cell {
-        fn update_state(mut self) {}
+        pub fn update_state(&mut self, neighbors: &[Cell]) {
+            self.previous_state = self.current_state.clone();
+
+            let mut alive_neighbors = 0;
+            for neighbor in neighbors.into_iter() {
+                if neighbor.current_state == State::Alive {
+                    alive_neighbors += 1;
+                }
+            }
+
+            self.current_state = match self.current_state {
+                State::Alive if alive_neighbors > 1 && alive_neighbors < 4 => State::Alive,
+                State::Alive => State::Dead,
+                State::Dead if alive_neighbors == 3 => State::Alive,
+                State::Dead => State::Dead,
+            };
+        }
+        fn get_previous_state(self) {}
+        fn get_neighbor_state(self) {}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use crustaceans_of_life::cell::Cell;
+use crustaceans_of_life::cell::State::Alive;
+use crustaceans_of_life::cell::State::Dead;
 fn main() {
-    println!("Hello, world!");
+    let mut tha_cell = Cell {
+        current_state: Dead,
+        previous_state: Dead,
+        position: (2, 2),
+    };
+    let mut _grid = [
+        Cell {
+            current_state: Alive,
+            previous_state: Dead,
+            position: (1, 1),
+        },
+        Cell {
+            current_state: Dead,
+            previous_state: Dead,
+            position: (2, 1),
+        },
+        Cell {
+            current_state: Alive,
+            previous_state: Dead,
+            position: (3, 1),
+        },
+        Cell {
+            current_state: Dead,
+            previous_state: Dead,
+            position: (1, 2),
+        },
+        Cell {
+            current_state: Alive,
+            previous_state: Dead,
+            position: (3, 2),
+        },
+        Cell {
+            current_state: Alive,
+            previous_state: Dead,
+            position: (1, 3),
+        },
+        Cell {
+            current_state: Dead,
+            previous_state: Dead,
+            position: (2, 3),
+        },
+        Cell {
+            current_state: Dead,
+            previous_state: Dead,
+            position: (3, 3),
+        },
+    ];
+
+    // Lets iterate over just 2 frames
+    tha_cell.update_state(&_grid);
+    println!(
+        "The state of the observed cell is {:?} \n",
+        tha_cell.current_state
+    );
+
+    tha_cell.update_state(&_grid);
+    println!(
+        "The state of the observed cell is {:?} \n",
+        tha_cell.current_state
+    );
 }
