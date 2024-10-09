f45d97ede8d1bc4469d40278e70f12cd43f3af4d
trujimat <mateo.trujillo@fing.edu.uy>
2024-10-08 23:27:02 -0300

    structure done, some testing and polishing is still needed

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod cell {
     pub struct Cell {
         pub current_state: State,
         pub previous_state: State,
-        pub position: (i32, i32),
+        pub position: (usize, usize),
     }
 
     #[derive(Debug, Clone, PartialEq)]
@@ -12,16 +12,15 @@ pub mod cell {
     }
 
     impl Cell {
-        pub fn update_state(&mut self, neighbors: &[Cell]) {
-            self.previous_state = self.current_state.clone();
-
-            let mut alive_neighbors = 0;
-            for neighbor in neighbors.into_iter() {
-                if neighbor.current_state == State::Alive {
-                    alive_neighbors += 1;
-                }
+        pub fn new(current_state: State, previous_state: State, position: (usize, usize)) -> Cell {
+            Cell {
+                current_state,
+                previous_state,
+                position,
             }
-
+        }
+        pub fn update_state(&mut self, alive_neighbors: u8) {
+            self.previous_state = self.current_state.clone();
             self.current_state = match self.current_state {
                 State::Alive if alive_neighbors > 1 && alive_neighbors < 4 => State::Alive,
                 State::Alive => State::Dead,
@@ -31,5 +30,94 @@ pub mod cell {
         }
         fn get_previous_state(self) {}
         fn get_neighbor_state(self) {}
+    }
+}
+
+pub mod grid {
+
+    use crate::cell::{Cell, State};
+
+    pub struct Grid {
+        rows: usize,
+        cols: usize,
+        cells: Vec<Vec<Cell>>,
+    }
+
+    impl Grid {
+        pub fn new(rows: usize, cols: usize) -> Grid {
+            let mut cells = Vec::new();
+            for x in 0..rows + 1 {
+                let mut row = Vec::new();
+                for y in 0..cols + 1 {
+                    row.push(Cell::new(State::Dead, State::Dead, (x, y)));
+                }
+                cells.push(row);
+            }
+            Grid { rows, cols, cells }
+        }
+
+        pub fn update_state(&mut self) {
+            for x in 0..self.rows + 1 {
+                for y in 0..self.cols + 1 {
+                    self.update_cell_state(x, y);
+                }
+            }
+        }
+
+        pub fn update_cell_state(&mut self, x: usize, y: usize) {
+            let dx = self.get_dx(x);
+            let dy = self.get_dy(y);
+            let alive_neighbors = self.get_alive_neighbors(x, y, dx, dy);
+
+            self.cells[x][y].update_state(alive_neighbors);
+        }
+
+        fn get_dx(&self, x: usize) -> (i8, i8) {
+            let mut dx = (-1, 1);
+            if x == 0 {
+                dx = (0, 1); // No left neighbor
+            } else if x == self.rows - 1 {
+                dx = (-1, 0); // No right neighbor
+            }
+            dx
+        }
+
+        fn get_dy(&self, y: usize) -> (i8, i8) {
+            let mut dy = (-1, 1);
+            if y == 0 {
+                dy = (0, 1); // No left neighbor
+            } else if y == self.cols - 1 {
+                dy = (-1, 0); // No right neighbor
+            }
+            dy
+        }
+
+        fn get_alive_neighbors(&self, x: usize, y: usize, dx: (i8, i8), dy: (i8, i8)) -> u8 {
+            let mut alive_neighbors = 0;
+            for x_offset in dx.0..dx.1 {
+                for y_offset in dy.0..dy.1 {
+                    let x_index = x as i8 + x_offset;
+                    let y_index = y as i8 + y_offset;
+                    if (x_index != 0 || y_index != 0)
+                        && self.cells[x_index as usize][y_index as usize].current_state
+                            == State::Alive
+                    {
+                        alive_neighbors += 1;
+                    }
+                }
+            }
+            alive_neighbors
+        }
+
+        pub fn print_state(&self) {
+            for x in 0..self.rows {
+                for y in 0..self.cols {
+                    println!(
+                        "The state of the observed cell {}{} is {:?} \n",
+                        x, y, self.cells[x][y].current_state
+                    );
+                }
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,9 @@
-use crustaceans_of_life::cell::Cell;
-use crustaceans_of_life::cell::State::Alive;
-use crustaceans_of_life::cell::State::Dead;
+use crustaceans_of_life::grid::{self, Grid};
 fn main() {
-    let mut tha_cell = Cell {
-        current_state: Dead,
-        previous_state: Dead,
-        position: (2, 2),
-    };
-    let mut _grid = [
-        Cell {
-            current_state: Alive,
-            previous_state: Dead,
-            position: (1, 1),
-        },
-        Cell {
-            current_state: Dead,
-            previous_state: Dead,
-            position: (2, 1),
-        },
-        Cell {
-            current_state: Alive,
-            previous_state: Dead,
-            position: (3, 1),
-        },
-        Cell {
-            current_state: Dead,
-            previous_state: Dead,
-            position: (1, 2),
-        },
-        Cell {
-            current_state: Alive,
-            previous_state: Dead,
-            position: (3, 2),
-        },
-        Cell {
-            current_state: Alive,
-            previous_state: Dead,
-            position: (1, 3),
-        },
-        Cell {
-            current_state: Dead,
-            previous_state: Dead,
-            position: (2, 3),
-        },
-        Cell {
-            current_state: Dead,
-            previous_state: Dead,
-            position: (3, 3),
-        },
-    ];
-
-    // Lets iterate over just 2 frames
-    tha_cell.update_state(&_grid);
-    println!(
-        "The state of the observed cell is {:?} \n",
-        tha_cell.current_state
-    );
-
-    tha_cell.update_state(&_grid);
-    println!(
-        "The state of the observed cell is {:?} \n",
-        tha_cell.current_state
-    );
+    let mut grid = Grid::new(3, 3);
+    grid.update_state();
+    grid.print_state();
+    println!("------------------------------------------------------------- \n");
+    grid.update_state();
+    grid.print_state()
 }
