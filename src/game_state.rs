0a46f65f8561e51f0180a6c3aa82c4c6303dd41f
trujimat <mateo.trujillo@fing.edu.uy>
2024-11-10 18:12:10 -0300

    added incomplete buttons functionality

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,9 +1,13 @@
 use crate::constants::DESIRED_FPS;
 use crate::grid::Grid;
+use ggez::event::{self, Button, MouseButton};
+use ggez::input::keyboard::{KeyCode, KeyInput};
 use ggez::*;
 pub struct GameState {
     dt: std::time::Duration,
     grid: Grid,
+    game_started: bool,
+    mouse_down: bool,
 }
 
 impl GameState {
@@ -14,17 +18,50 @@ impl GameState {
         let game_state = GameState {
             dt: std::time::Duration::new(0, 0),
             grid: grid,
+            game_started: false,
+            mouse_down: false,
         };
 
         game_state
     }
+
+    pub fn rec_to_index() {}
 }
 
 impl ggez::event::EventHandler<GameError> for GameState {
     fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
-        while _ctx.time.check_update_time(DESIRED_FPS) {
-            self.dt = _ctx.time.delta();
-            self.grid.update_state();
+        if self.game_started {
+            while _ctx.time.check_update_time(DESIRED_FPS) {
+                self.dt = _ctx.time.delta();
+                self.grid.update_state();
+            }
+        }
+        Ok(())
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        button: MouseButton,
+        x: f32,
+        y: f32,
+    ) -> GameResult {
+        self.mouse_down = true;
+        println!("Mouse button pressed: {button:?}, x: {x}, y: {y}");
+        // if !self.game_started {
+        //     self.get_indexes();
+        // }
+        Ok(())
+    }
+
+    fn key_down_event(&mut self, _ctx: &mut Context, input: KeyInput, repeat: bool) -> GameResult {
+        println!(
+            "Key pressed: scancode {}, keycode {:?}, modifier {:?}, repeat: {}",
+            input.scancode, input.keycode, input.mods, repeat
+        );
+        if input.keycode == Some(KeyCode::Space) && !self.game_started {
+            println!("Game Started!");
+            self.game_started = true;
         }
         Ok(())
     }
